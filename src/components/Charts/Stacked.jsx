9cd645b056e48bfde73933bb5aa507e11bf2a239
nbc7 <90021751+nbc7@users.noreply.github.com>
2022-08-15 15:33:54 -0300

    revenue section (stacked chart + sparkline chart)

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import {
+  Category,
+  ChartComponent,
+  Inject,
+  Legend,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  StackingColumnSeries,
+  Tooltip,
+} from '@syncfusion/ej2-react-charts';
 
-const Stacked = () => {
-  return <div>Stacked</div>;
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
+
+const Stacked = ({ height, width }) => {
+  return (
+    <ChartComponent
+      width={width}
+      height={height}
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      legendSettings={{ background: 'white' }}
+    >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export default Stacked;
